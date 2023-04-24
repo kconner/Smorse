3cd375a1be757230c8dd705431b63853ce8b9619
Kevin Conner <connerk@gmail.com>
2023-04-23 17:24:34 -0700

    Initial Commit

---
+++ Smorse/SmorseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmorseApp.swift
+//  Smorse
+//
+//  Created by Kevin Conner on 2023-04-23.
+//
+
+import SwiftUI
+
+@main
+struct SmorseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
