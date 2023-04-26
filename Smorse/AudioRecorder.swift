f214ea984f0f1fec972c2b6e74bcf291c617d169
Kevin Conner <connerk@gmail.com>
2023-04-25 23:33:35 -0700

    Noodling

--- Smorse/AudioRecorder.swift
+++ Smorse/AudioRecorder.swift
@@ -7,20 +7,57 @@
 
 import AVFoundation
 
-class AudioRecorder: NSObject, AVAudioRecorderDelegate {
+@MainActor
+class SampleStore: ObservableObject, SamplerDelegate {
     
-    private var task: Task<Void, Never>? = nil
+    @Published var power: Float = 0
+    
+    private var audioRecorder: Sampler?
+    
+    var enabled = false {
+        didSet {
+            guard enabled != oldValue else { return }
+            
+            if enabled {
+                if audioRecorder == nil {
+                    audioRecorder = try? Sampler(delegate: self)
+                }
+                
+                audioRecorder?.start()
+            } else {
+                audioRecorder?.stop()
+            }
+        }
+    }
+    
+    // MARK: - SamplerDelegate
+    
+    func samplerDelegateDidMeasure(power: Float) {
+        self.power = power
+    }
+    
+}
+
+@MainActor
+protocol SamplerDelegate: AnyObject {
+    func samplerDelegateDidMeasure(power: Float)
+}
+
+class Sampler: NSObject, AVAudioRecorderDelegate {
     
     private var audioRecorder: AVAudioRecorder?
+    private var task: Task<Void, Never>? = nil
     
-    func setUpAudioRecorder() {
+    private weak var delegate: SamplerDelegate?
+    
+    init(delegate: SamplerDelegate) throws {
+        self.delegate = delegate
+        
+        super.init()
+        
         let audioSession = AVAudioSession.sharedInstance()
-        do {
-            try audioSession.setCategory(.record, mode: .measurement, options: [.allowBluetooth])
-            try audioSession.setActive(true)
-        } catch {
-            print("Error setting up audio session: \(error)")
-        }
+        try audioSession.setCategory(.record, mode: .measurement, options: [.allowBluetooth])
+        try audioSession.setActive(true)
         
         let recordingSettings: [String: Any] = [
             AVFormatIDKey: Int(kAudioFormatLinearPCM),
@@ -29,59 +66,51 @@ class AudioRecorder: NSObject, AVAudioRecorderDelegate {
             AVEncoderAudioQualityKey: AVAudioQuality.low.rawValue
         ]
         
-        do {
-            let audioRecorder = try AVAudioRecorder(url: getAudioFileURL(), settings: recordingSettings)
-            audioRecorder.delegate = self
-            audioRecorder.isMeteringEnabled = true
-            audioRecorder.prepareToRecord()
-            self.audioRecorder = audioRecorder
-        } catch {
-            print("Error setting up audio recorder: \(error)")
-        }
-    }
-    
-    func getAudioFileURL() -> URL {
-        let fileManager = FileManager.default
-        let folder = fileManager.temporaryDirectory
-        return folder.appendingPathComponent("audio.m4a")
+        let url = FileManager.default.temporaryDirectory.appendingPathComponent("discard.m4a")
+        let audioRecorder = try AVAudioRecorder(url: url, settings: recordingSettings)
+        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        self.audioRecorder = audioRecorder
     }
     
     // MARK: - AVAudioRecorderDelegate
     
-    func startMonitoringLoudSounds() {
-        stopMonitoringLoudSounds()
-        
+    func start() {
         audioRecorder?.record()
         
-        task = Task {
-            await monitorLoudSounds()
+        task = Task.detached(priority: .utility) {
+            await self.monitorLoudSounds()
         }
     }
 
-    func monitorLoudSounds() async {
+    private func monitorLoudSounds(priorPower: Float = 0) async {
         guard let recorder = audioRecorder else { return }
         recorder.updateMeters()
 
         let power = recorder.averagePower(forChannel: 0)
-        let threshold: Float = -10.0 // Adjust this value based on your requirements
-
-        if power > threshold {
-            print("Loud sound detected")
-            // Add your reaction code here
+        // let threshold: Float = -10.0 // Adjust this value based on your requirements
+        
+        print("\(priorPower) -> \(power)")
+        
+        Task {
+            await MainActor.run {
+                self.delegate?.samplerDelegateDidMeasure(power: power)
+            }
         }
 
         do {
-            try await Task.sleep(nanoseconds: 5_000_000) // Sleep for 5 milliseconds (0.005 seconds)
+            try await Task.sleep(nanoseconds: 50_000_000) // 50 ms
         } catch {
             // Task was cancelled
             return
         }
         
         // I hope we get tail recursion
-        await monitorLoudSounds()
+        await monitorLoudSounds(priorPower: power)
     }
     
-    func stopMonitoringLoudSounds() {
+    func stop() {
         task?.cancel()
         task = nil
         
